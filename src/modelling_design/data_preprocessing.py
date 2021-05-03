5da6984cfaeea7b1105815c24961af1cc19918af
saraalgo <saraalvarezglez@gmail.com>
2021-05-03 17:04:55 +0200

    git fully upgraded with FS code

--- src/modelling_design/data_preprocessing.py
+++ src/modelling_design/data_preprocessing.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import json
 import pandas as pd
 import warnings
@@ -59,32 +58,3 @@ if __name__ == '__main__':
 config = open_json(CONFIG) 
 # Read csv files
 d_hist_features, d_descrip_featu = load_csv_files(config["Files with hist features"], config["Algorithm to extract features"], config["Files with descriptor features"], config["Number of descriptors selected"]) 
-=======
-import json
-
-# Omit warning messages from the following code
-warnings.filterwarnings("ignore")
-
-# Constant with the .json file path
-CONFIG = '../../config/data.json'
-
-# Load .json file
-def open_json(config_name):
-    """
-    Function to open json file and return data
-    :params: config_name - json file name
-    :return: json data
-    """
-    with open(config_name) as f:
-        return json.load(f)
-
-# Load .csv and fasta files
-def open_data(config_name):
-    
-
-
-
-def data_preprocessing():
-    # Read json
-    config = open_json(CONFIG) 
->>>>>>> 72ebaf79f5213169f270ae9e8d200b19781d4209
